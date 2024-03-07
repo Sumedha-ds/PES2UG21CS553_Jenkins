f383a2e1680d677b5f8aadc0892608d0eb237da6
sumedha-ds <sumedha.ds2003@gmail.com>
2024-03-07 22:25:14 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Hey !!! SUMEDHA D S (PES2UG21CS553)\n";
   return 0;
 }
